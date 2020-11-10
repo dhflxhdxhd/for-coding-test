a8638b32920a65361ad5ac0bb16bad42cadaa3e8
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2020-11-10 22:01:23 +0900

    Update optimalChange.py

--- Greedy/optimalChange.py
+++ Greedy/optimalChange.py
@@ -1,12 +1,13 @@
-# 그리디 greedy 알고리즘
-# 가장 큰 순서대로
-n = 1260
+n = int(input()) # 거슬러줘야 할 돈
 count = 0
 
-list = [500,100,50,10]
+# 그리디 알고리즘
+# 정당성) 가지고 있는 동전 중 큰 단위가 항상 작은 단위의 배수이므로.
+# 큰 단위부터
+array = [500, 100, 50, 10]
 
-for coin in list:
-    count += n
-    n %= coin
-
+for coin in array:
+    count += n // coin  # 거슬러 줄 수 있는 동전의 개수
+    n %= coin # 거슬러 주고 남은 돈n
+    
 print(count)
