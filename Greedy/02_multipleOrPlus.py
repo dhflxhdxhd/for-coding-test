15ac4b096b0f08f979b0cf46388bdd8204baeff6
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2020-11-13 22:54:57 +0900

    Update and Rename 01.py to 02_multipleOrPlus.py

---
+++ Greedy/02_multipleOrPlus.py
@@ -0,0 +1,15 @@
+// 곱하기 혹은 더하기
+// multiple or plus
+// point: num값이 1보다 같거나 작으면 더한다. result값이 1보다 작거나 같으면 더한다.
+s=input()
+result = int(s[0])
+
+if len(s)>=1 and len(s)<=20:
+    for i in range(1,len(s)):
+        num = int(s[i])
+        if num<=1 or result <= 1:
+            result += num
+        else:
+            result *= num
+
+print(result)
