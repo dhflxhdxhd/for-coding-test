1a4b5a8c540d5b41a7e6a08c0dd07860cf965c31
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2021-11-01 01:47:56 +0000

    2775.py 추가

--- baekjoon/2775.py
+++ baekjoon/2775.py
@@ -1,12 +1,22 @@
 # 부녀회장이 될테야
+# apt
+# 1 6 21 56 126
+# 1 5 15 35 70
+# 1 4 10 20 35
+# 1 3 6 10 15
+# 1 2 3 4 5 
 
-T=int(input())
+t = int(input())
 
-for _ in range(T):
-    k = int(input())
-    n = int(input())
-    rnum = [i for i in range(1,n+1)]
+for _ in range(t):  
+    k = int(input()) # k층
+    n = int(input()) # n호
+    apt = [i for i in range(1, n+1)] #apt 초기화
+    # print(apt)
+    
     for _ in range(k):
-        for j in range(1,n):
-            rnum[j] += rnum[j-1]
-    print(rnum[-1])        
+        for x in range(1,n):
+            apt[x] += apt[x-1]
+        # print(apt)
+    print(apt[-1]) #apt 가장 마지막 수
+   
