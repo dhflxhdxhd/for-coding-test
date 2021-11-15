ccb0ebe76d9a54073d06586715f6c00fc24dd8fa
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2021-11-15 02:03:51 +0000

    4948 출력 방법 추가

--- baekjoon/4948.py
+++ baekjoon/4948.py
@@ -1,6 +1,6 @@
 #베르트랑 공준
 #임의의 자연수 n에 대하여 n보다 크고 2n보다 작거나 같은 소수는 적어도 하나 존재한다.
-
+import sys
 import math
 def isPrime(num):
     if num==1:
@@ -11,15 +11,29 @@ def isPrime(num):
                 return False
         return True
 
-while True:
-    cnt = 0
-    n =  int(input())
-    if n==0:
+#여러 값에 대해 각각의 결과 출력 방법
+# 1)
+# while True:
+#     cnt = 0
+#     n =  int(input())
+#     if n==0:
+#         break
+    
+#     for i in range(n+1,2*n+1):
+#         if isPrime(i):
+#             cnt += 1
+            
+#     print(cnt)
+
+# 2)
+for num in sys.stdin:
+    cnt=0
+    num = int(num)
+    if num == 0:
         break
     
-    for i in range(n+1,2*n+1):
+    for i in range(num+1,2*num+1):
         if isPrime(i):
             cnt += 1
             
     print(cnt)
- 
