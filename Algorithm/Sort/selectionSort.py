6367d55d92b78d9b8dd716376254b469dff7ec40
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-01-18 08:01:21 +0000

    선택정렬 수정

--- Algorithm/Sort/selectionSort.py
+++ Algorithm/Sort/selectionSort.py
@@ -1,12 +1,12 @@
 array = [7,5,9,0,3,1,6,2,4,8]
 
-for i in range(len(array)):
+for i in range(len(array)-1): 
     min_index = i
     for j in range(i+1, len(array)):
         if array[min_index] > array[j]:
             min_index = j 
-    
     # Swap : 특정한 리스트가 주어졌을 때 두 변수의 위치를 변경하는 작업
     array[i], array[min_index] = array[min_index], array[i] 
+    print(array)
 
 print(array)
