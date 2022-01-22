1454da0b817448296e54791d74a9f60bd3d1f78e
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-01-22 06:30:36 +0000

    error > 정렬문제 > 10825

--- Sort/04_KEM.py
+++ Sort/04_KEM.py
@@ -4,7 +4,7 @@ arr = []
 for i in range(n):
     arr.append(input().split())
 
-array = sorted(arr, key=lambda score : (-int(score[1]), int(score[2]), -int(score[3]), score[0]) )
+arr.sort(key=lambda score : (-int(score[1]), int(score[2]), -int(score[3]), score[0]) )
 
-for a in array:
-    print(array[0])
+for a in arr:
+    print(a[0])
