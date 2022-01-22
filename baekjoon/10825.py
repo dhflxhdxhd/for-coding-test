14e8c87c3ec998d9a3fc824195c2200d3452efe3
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-01-22 11:12:46 +0000

    10825.py

---
+++ baekjoon/10825.py
@@ -0,0 +1,11 @@
+# 정렬
+n = int(input())
+
+arr = []
+for i in range(n):
+    arr.append(input().split())
+
+arr.sort(key=lambda score : (-int(score[1]), int(score[2]), -int(score[3]), score[0]) )
+
+for a in arr:
+    print(a[0])
