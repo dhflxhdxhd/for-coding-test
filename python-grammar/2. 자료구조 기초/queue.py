a918969cf59ae00d1be4445b406e0dc60fec074b
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-01-29 12:42:12 +0000

    deque

--- python-grammar/2. 자료구조 기초/queue.py
+++ python-grammar/2. 자료구조 기초/queue.py
@@ -1,3 +1,14 @@
+#deque : 양방향 큐(자료구조)
+#컨테이너(container)의 양끝 엘리먼트(element)에 접근하여 삽입 또는 제거를 할 경우, 일반적인 리스트(list)가 이러한 연산에 O(n)이 소요되는 데 반해, 데크(deque)는 O(1)로 접근 가능하다.
+#deque.append(item): item을 데크의 오른쪽 끝에 삽입한다.
+# deque.appendleft(item): item을 데크의 왼쪽 끝에 삽입한다.
+# deque.pop(): 데크의 오른쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.popleft(): 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.extend(array): 주어진 배열(array)을 순환하면서 데크의 오른쪽에 추가한다.
+# deque.extendleft(array): 주어진 배열(array)을 순환하면서 데크의 왼쪽에 추가한다.
+# deque.remove(item): item을 데크에서 찾아 삭제한다.
+# deque.rotate(num): 데크를 num만큼 회전한다(양수면 오른쪽, 음수면 왼쪽).
+
 from collections import deque #collections 모듈에서 제공하는 deque 자료구조 사용. 
 
 queue = deque()
