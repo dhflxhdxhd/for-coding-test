9e35e6c9f6af5d5934ef720a7715978241a035e9
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-02 13:33:00 +0000

    bfs 주석 추기

--- DFSBFS/bfs.py
+++ DFSBFS/bfs.py
@@ -5,17 +5,17 @@
 from collections import deque
 
 def bfs(graph,start,visited):
-    queue = deque([start])
-    visited[start] = True
+    queue = deque([start]) #deque()이용하여 queue 선언
+    visited[start] = True #시작 노드 방문처리
 
-    while queue:
-        v = queue.popleft()
+    while queue: # queue가 빌 때까지 반복
+        v = queue.popleft() # 가장 먼저 넣은 노드부터 선택
         print(v,end=" ")
         
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+        for i in graph[v]: # 선택한 노드에 인접한 노드들 탐색
+            if not visited[i]: #인접 노드 중 방문하지 않은 노드가 있다면
+                queue.append(i) # queue에 넣음
+                visited[i] = True # 그리고 방문처리
 
 graph = [
     [],
