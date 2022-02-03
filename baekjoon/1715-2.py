2176cfe97227d103ade5c760c1525026ff735310
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-03 13:30:53 +0000

    1715 카드정렬 add 주석

--- baekjoon/1715-2.py
+++ baekjoon/1715-2.py
@@ -1,4 +1,5 @@
 #카드 정렬하기
+#가장 작은 크기의 두 카드 묶음을 꺼내서 합치고 다시 삽입.
 #우선순위 큐 -> heap 이용
 import heapq
 n =  int(input())
@@ -9,10 +10,14 @@ for _ in range(n):
 
 result = 0
 while len(h) != 1:
+    #가장 작은 크기의 두 카드 꺼냄.
     one = heapq.heappop(h)
     two = heapq.heappop(h)
+    #두 카드 합
     sum_value = one + two
+    #최종 결과 값에 더함
     result += sum_value
+    #두 카드 합 다시 heap에 push
     heapq.heappush(h,sum_value)
 
 print(result)
