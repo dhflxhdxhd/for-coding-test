9f48360d932d61c1bea3f50b07a28894ccb4997e
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-03 13:19:26 +0000

    heap sort 2

--- python-grammar/heap.py
+++ python-grammar/heap.py
@@ -2,7 +2,7 @@
 #heapq.heappush()
 #heapq.heappop()
 
-#힙정렬
+#힙정렬 1 
 import heapq
 
 def heapsort(iterable):
@@ -10,6 +10,7 @@ def heapsort(iterable):
     result = []
     for value in iterable:
         heapq.heappush(h,value)
+        # print(h)
     
     for i in range(len(h)):
         result.append(heapq.heappop(h))
@@ -18,3 +19,8 @@ def heapsort(iterable):
 arr = [1,3,5,7,9,2,4,6,8,0]
 result = heapsort(arr)
 print(result)
+
+#힙 정렬 2
+# arr = [1,3,5,7,9,2,4,6,8,0]
+# heapq.heapify(arr)
+# print(arr)
