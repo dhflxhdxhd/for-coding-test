059976c61558c54f37e3b473cf6706dc7eeda7d8
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-04 06:44:55 +0000

    2606.py DFS 이용

---
+++ baekjoon/2606.py
@@ -0,0 +1,23 @@
+#virus
+#DFS/BFS
+
+def dfs(graph,v,visited):
+    global cnt
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            cnt += 1
+            dfs(graph,i,visited)
+
+com = int(input())
+net = int(input())
+graph = [[]*com for _ in range(com+1)]
+for i in range(net):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+cnt = 0
+visited = [False] * (com+1)
+
+dfs(graph,1,visited)
+print(cnt)
