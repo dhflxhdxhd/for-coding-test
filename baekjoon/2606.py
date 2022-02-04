f4f176008c5284197cfdfc0c492bb4c5fa876e94
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-04 07:10:23 +0000

    2606 refactor

--- baekjoon/2606.py
+++ baekjoon/2606.py
@@ -9,16 +9,19 @@ def dfs(graph,v,visited):
             cnt += 1
             dfs(graph,i,visited)
 
+
 com = int(input())
 net = int(input())
 graph = [[]*com for _ in range(com+1)]
+
 for i in range(net):
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
+
 cnt = 0
 visited = [False] * (com+1)
 
 dfs(graph,1,visited)
-print(count)
+print(cnt)
 # print(visited.count(True)-1)
