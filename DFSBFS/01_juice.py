48ff3ba9f95182146094bc43bb4d5b7cad6cb92f
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-06 13:35:07 +0000

    . error

---
+++ DFSBFS/01_juice.py
@@ -0,0 +1,26 @@
+n,m = map(int,input().split()) #세로 n 가로 m
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int,input())))
+
+def dfs(x,y):
+    if x <= -1 or x >= m or y <= -1 or y >= n:
+        return False
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+        return True
+    return False
+
+result = 0
+for i in range(m):
+    for j in range(n):
+        if dfs(i,j) == True:
+            result += 1
+
+print(result)
