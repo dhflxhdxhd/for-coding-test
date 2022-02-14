4609699de319a60f3d3f3b31ce0212092cc3b349
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-14 13:19:39 +0000

    18352.py make graph

---
+++ baekjoon/18352.py
@@ -0,0 +1,12 @@
+input_data = list(map(int,input().split()))
+
+n = input_data[0]
+m = input_data[1]
+k = input_data[2]
+x = input_data[-1]
+
+graph = []
+for _ in range(m):
+    graph.append(input().split())
+
+
