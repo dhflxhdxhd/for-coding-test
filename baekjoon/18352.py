a9c047f09610ce55ce1ffe507d331a3b5be4c21b
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-14 13:29:11 +0000

    18352.py input

--- baekjoon/18352.py
+++ baekjoon/18352.py
@@ -5,8 +5,9 @@ m = input_data[1]
 k = input_data[2]
 x = input_data[-1]
 
-graph = []
+graph = [[] for _ in range( n+1) ]
 for _ in range(m):
-    graph.append(input().split())
+    a,b = map(int, input().split())
+    graph[a].append(b)
 
 
