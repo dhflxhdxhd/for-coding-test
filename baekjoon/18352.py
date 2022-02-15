437fdecf06c36e974e5173637b447c58ff804960
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-15 12:49:08 +0000

    다익스트라

--- baekjoon/18352.py
+++ baekjoon/18352.py
@@ -1,3 +1,7 @@
+# 그래프 이론
+# 그래프 탐색
+# 너비 우선 탐색
+# 다익스트라
 input_data = list(map(int,input().split()))
 
 n = input_data[0]
@@ -5,9 +9,23 @@ m = input_data[1]
 k = input_data[2]
 x = input_data[-1]
 
-graph = [[] for _ in range( n+1) ]
+graph = [[] for _ in range(n+1) ]
 for _ in range(m):
     a,b = map(int, input().split())
     graph[a].append(b)
 
+from collections import deque
 
+visited = [] * (???????????)
+def bfs(start,visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+
+        if graph[v]:
+            for i in graph[v]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
