b2645725039d457bc3a6e4d6da023004df9fb181
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-02-23 10:38:07 +0000

    11650

--- baekjoon/11650.py
+++ baekjoon/11650.py
@@ -2,4 +2,5 @@ n = int(input())
 
 arr = [list(map(int,input().split())) for _ in range(n)]
 
-arr = sorted(arr, key=lambda arr[0][0])
+arr = sorted(arr, key=lambda arr: (arr[0],arr[1]))
+print(arr)
