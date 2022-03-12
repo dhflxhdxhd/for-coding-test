2b6caf0f402e5d1b2bca18a21cd718c1a6fb4d79
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-03-12 13:39:45 +0000

    1978.py

--- baekjoon/1978.py
+++ baekjoon/1978.py
@@ -1,15 +1,20 @@
 #소수찾기
-
 n = int(input())
-nums = map(int, input().split())
+nums = list(map(int, input().split()))
+cnt = 0
 
-for num in nums:
+def prime(num):
     if num == 1:
         return False
     elif num == 2:
         return True
     
-    for i in range(2, num)
+    for i in range(2, num):
         if num % i ==0 :
             return False
     return True
+
+for num in nums :
+    if prime(num):
+        cnt += 1
+print(cnt)
