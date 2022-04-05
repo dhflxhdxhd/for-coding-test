60c4ace75d6c3f465934ec793687ba8bc18e1404
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-04-05 12:45:37 +0000

    문자열정렬

---
+++ Sort/str_sort.py
@@ -0,0 +1,23 @@
+# str = ["119", "97674223", "1195524421"]
+# str = ["12", "123", "1235", "567", "88"]
+# str.sort()
+# print(str)
+
+
+# p = ["leo", "dave","kiki", "eden"]
+# c = ["eden", "kiki"]
+# p.sort()
+# c.sort()
+
+# def sol(p,c):
+#     p.sort()
+#     c.sort()
+#     for i in range(len(c)):
+#         if (p[i] != c[i]):
+#             return p[i]
+
+#     return p[-1]
+
+print(p[-1])
+# print(p)
+# print(c)
