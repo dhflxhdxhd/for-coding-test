f7e64b383403454deb2e106b478827dced5bd490
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-04-06 03:27:17 +0000

    error 프린터.py

---
+++ programmars/StackandQueue/print.py
@@ -0,0 +1,26 @@
+# 프린터
+
+def solution(priorities, location):
+    answer = []
+    num_index = [0,1,2,3] 
+    idx = 0
+    
+    while priorities:
+        check = True
+        num = priorities.pop(0)
+        for p in priorities:
+            if num < p:
+                priorities.append(num)
+                idx = num_index.pop(0)
+                num_index.append(idx)
+                check = False
+                break
+        if check:
+            idx = num_index.pop(0)
+            answer.append(idx)
+
+        for ans in answer:
+            if ans == location:
+                return answer.index(ans) + 1
+    
+print(solution([1,1,9,1,1,1],0))
