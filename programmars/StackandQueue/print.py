45c1c465207b17d2b485fa272146ed07e6336b8b
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-04-06 03:39:04 +0000

    modify print.py

--- programmars/StackandQueue/print.py
+++ programmars/StackandQueue/print.py
@@ -1,5 +1,4 @@
 # 프린터
-
 def solution(priorities, location):
     answer = []
     num_index = [i for i in range(len(priorities))]
@@ -11,16 +10,27 @@ def solution(priorities, location):
         for p in priorities:
             if num < p:
                 priorities.append(num)
-                idx = num_index.pop(0)
-                num_index.append(idx)
+                num_index.append(num_index.pop(0))
                 check = False
                 break
         if check:
-            idx = num_index.pop(0)
-            answer.append(idx)
+            answer.append(num_index.pop(0))
 
-        for ans in answer:
-            if ans == location:
-                return answer.index(ans) + 1
-    
+    return answer.index(location) + 1
+
+# def solution(priorities, location):
+#     answer = []
+#     num_loc = [i for i in range(len(priorities))]
+ 
+#     while priorities:
+#         if priorities[0] == max(priorities):
+#             answer.append(num_loc.pop(0))
+#             priorities.pop(0)
+#         else:
+#             priorities.append(priorities.pop(0))
+#             num_loc.append(num_loc.pop(0))
+
+#     return answer.index(location) + 1
+
+
 print(solution([1,1,9,1,1,1],0))
