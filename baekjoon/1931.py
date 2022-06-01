f1dee8db4afb3f86bdd68d7ff3bac583f80d154e
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-06-01 13:04:54 +0000

    1931

--- baekjoon/1931.py
+++ baekjoon/1931.py
@@ -1,6 +1,6 @@
 #1931
 #회의실 배정
-#error
+
 n = int(input())
 time_list = []
 
@@ -8,13 +8,15 @@ for i in range(n):
     x,y = map(int,input().split())
     time_list.append([x,y])
 
-time_list.sort(key = lambda x : (x[0], x[1]))
+time_list.sort(key = lambda x : (x[1], x[0]))
+print(time_list)
+count = 0
+end_time = 0
 
-cnt = 0 
-end = 0
-for s,e in time_list:
-    if s  >= end:
-        cnt += 1
-        end = e
-    
-print(cnt)
+for start, end in time_list:
+    if start >= end_time:
+        # print([start,end])
+        count += 1
+        end_time = end 
+
+print(count)
