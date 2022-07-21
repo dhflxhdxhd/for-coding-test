912116bf874c8f36ffe3aa05b1e0ebc9a98c72e8
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-07-21 11:29:06 +0000

    1764.py

--- baekjoon/10816.py
+++ baekjoon/10816.py
@@ -2,13 +2,13 @@ m = int(input())
 mums = list(map(int,input().split()))
 n = int(input())
 nums = list(map(int,input().split()))
-# count = 0
-# for num in nums:
-#     for mum in mums:
-#         if num == mum:
-#             count += 1
-#     print(count,end=' ')
-#     count = 0
+count = 0
+for num in nums:
+    for mum in mums:
+        if num == mum:
+            count += 1
+    print(count,end=' ')
+    count = 0
 
 dict = dict()
 
