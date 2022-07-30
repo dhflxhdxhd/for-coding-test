6af84ac8b10ac314d2cb1177700d6bb15afbc023
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-07-30 11:46:48 +0000

    DP 9461 탑다운 방식 추가

--- baekjoon/9461.py
+++ baekjoon/9461.py
@@ -1,5 +1,6 @@
 # 9461. 파도반수열
 
+# 방법 1
 t = int(input())
 
 d = [0] * 101
@@ -10,6 +11,28 @@ def showP(n):
     for i in range(6, n+1):
         d[i] = d[i-1] + d[i-5]
 
+    return d[n]
+
+for i in range(t):
+    n = int(input())
+    print(showP(n))
+
+
+# 방법 2
+t = int(input())
+d = [0] * 101
+
+def showP(n):
+    if n == 1:
+        return 1
+        
+    if n >= 2 and n <= 5:
+        return n // 2
+
+    if d[n] != 0:
+        return d[n]
+
+    d[n] = showP(n-1) + showP(n-5)
     return d[n]
 
 for i in range(t):
