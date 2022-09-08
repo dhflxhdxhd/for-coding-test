3d8e85f7e1bfcc1ad0e62bc9cf9b2fc1939548e8
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-09-08 02:18:26 +0000

    순위검색  ing

---
+++ init.py
@@ -0,0 +1,21 @@
+# 순위 검색
+
+
+def solution(info, query):
+    info_list = [([] for i in range(5)) for i in range(len(info))]
+    query_list = [([] for i in range(5)) for i in range(len(info))]
+    
+    for i in range(len(info)):
+        info_list[i] = info[i].split(" ")
+
+    for j in range(len(query)):
+        query_list[j] = query[j].split(" ")
+
+
+info = ["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
+query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
+
+print(solution(info,query))
+
+
+
