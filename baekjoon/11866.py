0af5c969bc9e60ffeb1f8b1a09f71fe6986c111c
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-10-05 00:39:54 +0000

    11866 deque 이용

--- baekjoon/11866.py
+++ baekjoon/11866.py
@@ -1,3 +1,4 @@
+# 방법 1
 n,k = map(int,input().split())
 queue = [i+1 for i in range(n)]
 
@@ -13,6 +14,29 @@ for i in range(n,0,-1):
         print(queue.pop(idx), end="")
 
 print(">")
+
+
+# 방법 2
+from collections import deque
+
+n, k = map(int, input().split())
+list = [i+1 for i in range(n)]
+
+queue = deque(list)
+ans = []
+
+while queue:
+    for i in range(k-1):
+        queue.append(queue.popleft())
+
+    ans.append(queue.popleft())
+
+
+print("<", end = "")
+for i in range(n-1):
+    print(ans[i], end=", ")
+
+print(f"{ans[-1]}>")
 
 
 
