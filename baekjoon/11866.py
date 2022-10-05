ce83f0a3112b52f3285638e26206fe25a0fc95f5
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-10-05 00:34:52 +0000

    11866 index 이용

--- baekjoon/11866.py
+++ baekjoon/11866.py
@@ -1,22 +1,19 @@
-from collections import deque
-
-n, k = map(int, input().split())
-list = [i+1 for i in range(n)]
-
-queue = deque(list)
-ans = []
-
-while queue:
-    for i in range(k-1):
-        queue.append(queue.popleft())
-
-    ans.append(queue.popleft())
+n,k = map(int,input().split())
+queue = [i+1 for i in range(n)]
 
 
-print("<", end = "")
-for i in range(n):
-    print(ans[i], end=", ")
+idx = 0
+print("<", end="")
+for i in range(n,0,-1):
+    idx = (idx + (k-1)) % i
+
+    if len(queue) > 1:
+        print(queue.pop(idx), end=", ")
+    else:
+        print(queue.pop(idx), end="")
+
 print(">")
+
 
 
 
---
+++ init.py
@@ -0,0 +1,15 @@
+n,k = map(int,input().split())
+queue = [i+1 for i in range(n)]
+
+
+idx = 0
+print("<", end="")
+for i in range(n,0,-1):
+    idx = (idx + (k-1)) % i
+
+    if len(queue) > 1:
+        print(queue.pop(idx), end=", ")
+    else:
+        print(queue.pop(idx), end="")
+
+print(">")
