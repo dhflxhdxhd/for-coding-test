e8e2f4423d16bccfa66d5ab55f500a988c799926
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-10-13 06:02:53 +0000

    5430 AC solve

--- baekjoon/5430.py
+++ baekjoon/5430.py
@@ -4,31 +4,32 @@ import sys
 t = int(input())
 
 for i in range(t):
-
     command = sys.stdin.readline()
     n = int(input())
-    # nums = list(map(int,input()[1:-1].split(',')))
-    nums = sys.stdin.readline()[1:-1].split(',')
+    nums = sys.stdin.readline().rstrip()[1:-1].split(',')
     queue = deque(nums)
     
     if n == 0:
         queue = deque()
 
-    
+    reverse = 0
     check = 1
     for cmd in command:
         if cmd == "R":
-            queue.reverse()
+            reverse += 1
         elif cmd == "D":
             if queue:
-                queue.popleft()
+                if reverse % 2 == 0:
+                    queue.popleft()
+                else:
+                    queue.pop()
             else:
                 print("error")
                 check = 0
                 break
 
     if check == 1:
+        if reverse % 2 == 1:
+            queue.reverse()
         print("["+",".join(list(queue))+"]")
  
-
-
