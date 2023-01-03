14ea6f46dbc8c716fa71f60f78903e73e608d885
or.llon <dhflxhdxhd@gmail.com>
2023-01-03 14:37:24 +0900

    Rename : 그리디 문제 파일 이름 변경

--- categories/Greedy/multipleOrPlus.py
+++ categories/Greedy/multipleOrPlus.py
@@ -1,6 +1,6 @@
-// 곱하기 혹은 더하기
-// multiple or plus
-// point: num값이 1보다 같거나 작으면 더한다. result값이 1보다 작거나 같으면 더한다.
+# 곱하기 혹은 더하기
+# multiple or plus
+# point: num값이 1보다 같거나 작으면 더한다. result값이 1보다 작거나 같으면 더한다.
 s=input()
 result = int(s[0])
 
