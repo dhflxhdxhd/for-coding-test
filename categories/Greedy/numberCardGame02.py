d4199b4b2eb6f9e45cc5bfd28c6e7799927de0ed
or.llon <dhflxhdxhd@gmail.com>
2023-01-03 15:01:38 +0900

    Add : [이코테] 그리디 - 숫자 카드 게임

---
+++ categories/Greedy/numberCardGame02.py
@@ -0,0 +1,14 @@
+# 이코테
+# 숫자 카드 게임
+# 가장 작은 수의 모임 중 가장 큰 수
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    cards = map(int,input().split())
+    minCard = min(cards)
+    result = max(result,minCard)
+
+print(result);
+
+    
