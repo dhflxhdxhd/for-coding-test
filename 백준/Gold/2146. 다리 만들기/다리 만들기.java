6e92e6a1d33a142697a8958f246ab4444baf141c
or_hyn <52151533+dhflxhdxhd@users.noreply.github.com>
2023-10-11 11:44:41 +0900

    [Gold III] Title: 다리 만들기, Time: 292 ms, Memory: 138356 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,129 +1,130 @@
 import java.io.*;
 import java.util.*;
 
-class Inode{
-	int row;
-	int col;
+// 2146. 다리 만들기
+
+class NodeD{
+	int r;
+	int c;
 	int dist;
-	public Inode(int row, int col) {
-		super();
-		this.row = row;
-		this.col = col;
-	}
 	
-	public Inode(int row, int col, int dist) {
+	public NodeD(int r, int c) {
 		super();
-		this.row = row;
-		this.col = col;
+		this.r = r;
+		this.c = c;
+	}
+
+	public NodeD(int r, int c, int dist) {
+		super();
+		this.r = r;
+		this.c = c;
 		this.dist = dist;
-	} 
+	}
 }
 
-// 2146. 다리만들기
+
 public class Main {
 	
-
 	static boolean[][] visited;
-	static int[][] map;
-	static int n;
+	static int n, map[][],minDist ;
+	static Queue<NodeD> queue = new LinkedList<NodeD>();
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine()); // 지도의 크기 
+		StringTokenizer st = null;
+		n = Integer.parseInt(br.readLine()); // 지도의 크기
 		
-		// 지도 입력
 		map = new int[n][n];
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		// 섬 구별하기 
-		int cnt = 2;
+		int num = 2;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if(map[i][j] == 1) {
-					checkIsland(i,j,cnt);
-					cnt++;
+					makeIsland(i, j, num++);
 				}
 			}
 		}
 		
+		minDist = Integer.MAX_VALUE;
+	
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if(map[i][j] >= 2) {
-					visited = new boolean[n][n];
-					countBridge(i,j);
+					getBridge(i,j, map[i][j]);
 				}
 			}
 		}
 		
 		System.out.println(minDist);
-	}	
-	
+	}
+
 	
 	// 상우하좌
 	static int[] dr = {-1,0,1,0};
 	static int[] dc = {0,1,0,-1};
-	static int minDist = Integer.MAX_VALUE;
-	static void countBridge(int row, int col) {
-		Queue<Inode> queue2 = new LinkedList<>();
-		queue2.offer(new Inode(row, col, 0));
-		int num = map[row][col];
+	static void getBridge(int r, int c, int num) {
+		visited = new boolean[n][n];
+		queue.clear();
+		queue.offer(new NodeD(r, c,0));
+		visited[r][c] = true;
 		
-		while (!queue2.isEmpty()) {
-			Inode inode = queue2.poll();
-			visited[inode.row][inode.col] = true;
+		while (!queue.isEmpty()) {
+			NodeD temp = queue.poll();
 			
-			// 가지치기
-			if(inode.dist >=  minDist) {
-				return;
+			if(minDist < temp.dist) {
+				continue;
 			}
-						
+			
+			
+			
 			for (int d = 0; d < 4; d++) {
-				int nr = inode.row + dr[d];
-				int nc = inode.col + dc[d];
+				int nr = temp.r + dr[d];
+				int nc = temp.c + dc[d];
 				
-				// 다리 놓기 (경계 안에 있고 && 방문하지 않은 곳이라면)
 				if(inArea(nr, nc) && !visited[nr][nc]) {
 					visited[nr][nc] = true;
-					// 바다라면 다리 1 증가
+
+					// 바다이면 다리 길이++;
 					if(map[nr][nc] == 0) {
-						queue2.offer(new Inode(nr, nc, inode.dist+1));
-					// 육지 && 이동 전 대륙의 번호랑 다르면
-					}else if(map[nr][nc] != num){
-						minDist = Math.min(minDist, inode.dist);
+						queue.offer(new NodeD(nr, nc, temp.dist+1));
+
+					// 처음 시작한 섬과 다른 섬이라면 다리 연결 O
+					// 가장 먼저 도달했을 때가 제일 짧은 길이
+					}else if(map[nr][nc] != num ) {
+						minDist = Math.min(temp.dist, minDist);
 					}
-					
+				}
+			}
+		}
+	}
+
+	static void makeIsland(int r, int c, int num) {
+		queue.clear();
+		queue.offer(new NodeD(r, c));
+		
+		while (!queue.isEmpty()) {
+			NodeD temp = queue.poll();
+			map[temp.r][temp.c] = num;
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = temp.r + dr[d];
+				int nc = temp.c + dc[d];
+				
+				if(inArea(nr, nc) && map[nr][nc] == 1) {
+					map[nr][nc] = num;
+					queue.offer(new NodeD(nr, nc));
 				}
 			}
 		}
 	}
 	
-
-	static void checkIsland(int row, int col, int cnt) {
-		Queue<Inode> queue = new LinkedList<>();
-		queue.offer(new Inode(row, col));
-		map[row][col] = cnt;
-		
-		while (!queue.isEmpty()) {
-			Inode inode = queue.poll();
-			
-			for (int d = 0; d < 4; d++) {
-				int nr = inode.row + dr[d];
-				int nc = inode.col + dc[d];
-				
-				if(inArea(nr, nc) && map[nr][nc] == 1) {
-					map[nr][nc] = cnt;
-					queue.offer(new Inode(nr, nc));
-				}
-			}
-		}
+	static boolean inArea(int r, int c) {
+		return r >= 0 && r < n && c >= 0 && c < n;
 	}
-
-	// 경계 안에 있는지 판단하는 메소드
-	static boolean inArea(int row, int col) {
-		return row >= 0 && row < n && col >= 0 && col < n;
-	}
+	
 }
